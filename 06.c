0e576494475ed7caf3366ef8af3d271e1b8e28f9
Amal Poulose <amalpoulose1995@hotmail.com>
2017-10-20 01:09:30 +0530

    Add files via upload

---
+++ 06.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+#include<errno.h>
+int main(void)
+{
+	int i=0;
+	while(i<3)
+	{
+		if(fork()==0)
+		{
+			printf("child %d and id = %d parent id = %d\n",i+1,getpid(),getppid());
+			break;
+		}
+		else
+		i++;
+	}
+	return 0;
+}
